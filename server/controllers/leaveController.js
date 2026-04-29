cddd1ad6f3b757b9c24758d98639571ab962e8ee
Chr1sHB <cristian_hb2000@hotmail.com>
2026-04-29 01:49:49 -0600

    Primer commit

---
+++ server/controllers/leaveController.js
@@ -0,0 +1,124 @@
+import Employee from '../models/Employee.js'
+import Leave from '../models/Leave.js'
+
+const addLeave = async (req, res) => {
+    try {
+        const {userId, leaveType, startDate, endDate, reason} = req.body
+        const employee = await Employee.findOne({userId})
+
+        const newStart = new Date(startDate);
+        const newEnd   = new Date(endDate);
+
+        if (newEnd < newStart) {
+            return res.status(400).json({ message: 'La fecha de fin debe ser posterior a la de inicio' });
+        }
+        console.log("leave")
+
+        const overlappingLeave = await Leave.findOne({
+            status: { $nin: ['Rejected'] },
+            startDate: { $lte: newEnd },
+            endDate:   { $gte: newStart }
+        });
+
+        if (overlappingLeave) {
+        return res.status(500).json({
+            error: 'El rango de fechas se solapa con una solicitud existente.',
+            conflictingLeave: overlappingLeave  
+        });
+        }
+
+        const newLeave = new Leave({
+            employeeId: employee._id, leaveType, startDate, endDate, reason
+        })
+
+
+        await newLeave.save()
+
+        return res.status(200).json({success: true})
+
+    } catch(error) {
+        console.log(error.message)
+        return res.status(500).json({success: false, error: "leave add server error"})
+    }
+}
+
+const getLeave = async (req, res) => {
+    try {
+        const {id, role} = req.params;
+        let leaves
+        if(role === "admin") {
+            leaves = await Leave.find({employeeId: id})
+        } else {
+            const employee = await Employee.findOne({userId: id})
+            leaves = await Leave.find({employeeId: employee._id})
+        }
+        
+        return res.status(200).json({success: true, leaves})
+    } catch(error) {
+        console.log(error.message)
+        return res.status(500).json({success: false, error: "leave add .. server error"})
+    }
+}
+
+const getLeaves = async (req, res) => {
+    try {
+        const leaves = await Leave.find().populate({
+            path: 'employeeId',
+            populate: [
+                {
+                    path: 'department',
+                    select: 'dep_name'
+                },
+                {
+                    path: 'userId',
+                    select: 'name'
+                }
+            ]
+        })
+
+        return res.status(200).json({success: true, leaves})
+    } catch(error) {
+        console.log("Eror: ", error.message)
+        return res.status(500).json({success: false, error: "leave add server error"})
+    }
+}
+
+const getLeaveDetail = async (req, res) => {
+    try {
+        const {id} = req.params;
+        const leave = await Leave.findById({_id: id}).populate({
+            path: 'employeeId',
+            populate: [
+                {
+                    path: 'department',
+                    select: 'dep_name'
+                },
+                {
+                    path: 'userId',
+                    select: 'name profileImage'
+                }
+            ]
+        })
+ 
+        return res.status(200).json({success: true, leave})
+    } catch(error) {
+        console.log(error.message)
+        return res.status(500).json({success: false, error: "leave detail server error"})
+    }
+}
+
+const updateLeave = async (req, res) => {
+    try {
+        const {id} = req.params;
+        const leave = await Leave.findByIdAndUpdate({_id: id}, {status: req.body.status})
+        if(!leave) {
+        return res.status(404).json({success: false, error: "leave not founded"})
+        }
+        return res.status(200).json({success: true})
+    } catch(error) {
+        console.log(error.message)
+        return res.status(500).json({success: false, error: "leave update server error"})
+    }
+}
+
+export {addLeave, getLeave, getLeaves, getLeaveDetail, updateLeave}
---
+++ server/index.js
@@ -0,0 +1,42 @@
+import express from 'express'
+import path from 'path';
+import { fileURLToPath } from 'url';
+import cors from 'cors'
+import authRouter from './routes/auth.js'
+import departmentRouter from './routes/department.js'
+import employeeRouter from './routes/employee.js'
+import salaryRouter from './routes/salary.js'
+import leaveRouter from './routes/leave.js' 
+import settingRouter from './routes/setting.js'
+import attendanceRouter from './routes/attendance.js'
+import dashboardRouter from './routes/dashboard.js'
+import connectToDatabase from './db/db.js'
+
+
+connectToDatabase() 
+const app = express() 
+app.use(cors())
+app.use(express.json())
+app.use(express.static('public/uploads'))
+app.use('/api/auth', authRouter)
+app.use('/api/department', departmentRouter)
+app.use('/api/employee', employeeRouter)
+app.use('/api/salary', salaryRouter)
+app.use('/api/leave', leaveRouter)
+app.use('/api/setting', settingRouter)
+app.use('/api/attendance', attendanceRouter)
+app.use('/api/dashboard', dashboardRouter)
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is Running on port ${process.env.PORT}`)
+})
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, '..', 'frontend', 'dist')));
+
+// Para que React maneje las rutas del lado cliente (SPA)
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'frontend', 'dist', 'index.html'));
+});
